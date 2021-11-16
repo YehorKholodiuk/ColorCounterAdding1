105cbe03d52d2345957dd5503e7ebabdc31235b0
ASUS <Yehor Kholodiuk>
2021-11-16 18:12:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import { nanoid } from 'nanoid'
+
+const initialTableData2 = [
+    {id: nanoid(), cols: [ {id: nanoid()}, {id: nanoid()} ] },
+    {id: nanoid(), cols: [ {id: nanoid()}, {id: nanoid()} ] },
+    {id: nanoid(), cols: [ {id: nanoid()}, {id: nanoid()} ] }
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [columns, setColumns] = useState(2)
+    const [rows, setRows] = useState(3)
+
+    const [tableData, setTableData] = useState(initialTableData2)
+
+    const generateTableData = (c, r) => {
+        const table = [];
+        for(let row = 1; row <= r; row++){
+            const tempRow = [];
+            for(let col = 1; col <= c; col++){
+                tempRow.push({ id: nanoid()})
+            }
+            table.push({ id: nanoid(), cols: tempRow })
+        }
+
+        setTableData(table)
+    };
+
+    const minusColumns = () => {
+        if(columns > 1) setColumns(columns - 1)
+        generateTableData(columns - 1, rows)
+    };
+
+    const plusColumns = () => {
+        setColumns(columns + 1)
+        generateTableData(columns + 1, rows)
+    };
+
+    const minusRows = () => {
+        if(rows > 1) setRows(rows - 1)
+        generateTableData(columns, rows - 1)
+    };
+
+    const plusRows = () => {
+        setRows(rows + 1)
+        generateTableData(columns, rows +1)
+    };
+
+
+    return (
+        <div className="App">
+            <button onClick={minusRows}>Minus row</button>
+            {rows}
+            <button onClick={plusRows}>Minus row</button>
+
+            <hr/>
+
+            <button onClick={minusColumns}>Minus col</button>
+            {columns}
+            <button onClick={plusColumns}>Minus col</button>
+
+            <hr/>
+
+
+            <table border={1}>
+                <tbody>
+
+                {tableData.map((row) => <tr key={row.id}>
+                    {row.cols.map(col => <td key={col.id}>*</td>)}
+                </tr>)}
+
+                </tbody>
+            </table>
+
+        </div>
+    );
 }
 
-export default App;
+export default App
